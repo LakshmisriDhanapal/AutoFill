74b897e01d14a0cf025715658667679067310b88
Lakshmi Sri <142877892+LakshmisriDhanapal@users.noreply.github.com>
2025-09-10 22:02:53 +0530

    test_mongo.py

--- test_mongo.py
+++ test_mongo.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb/")
 db = client["ocr_database"]
 collection = db["extracted_data"]
 
 # Print all documents
 for doc in collection.find():
     print(doc)
+
